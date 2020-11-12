c9464a5419319e18e9c418a2095d5b8e3954e17e
Chus <takalong@tutanota.com>
2020-11-12 12:31:53 +0100

    Primera version TaskButton

---
+++ src/TaskButtom.svelte
@@ -0,0 +1,41 @@
+<script>
+
+$ projects = { name: false };
+
+async function startTask(){
+
+		projects.name = !projects.name;
+};
+
+/*
+async function endTask(){
+
+}
+*/
+</script>
+
+<main>
+
+{#if projects.name}
+
+	<button class="start" on:click={startTask}>
+		Start Task
+	</button>
+{/if}
+
+{#if !projects.name}
+
+	<button class="end" on:click={startTask}>
+	    End Task
+	</button>
+{/if}
+
+
+<!--
+<button class="start" on:click={startTask}>Start Task</button>
+<button class="end" on:click={endTask}>End Task</button>
+-->
+</main>
+<style>
+
+</style>
