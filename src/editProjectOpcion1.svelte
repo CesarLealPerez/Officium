d27baf4d9c63995708f9186849238439d527cbc2
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2020-11-17 18:47:22 +0100

    Dos propuestas editProjectOpcion[1,2].svelte

---
+++ src/editProjectOpcion1.svelte
@@ -0,0 +1,37 @@
+<script>
+/**
+ * En esta opción los cambios se guardan sólo al hacer click en el botón guardar.
+ */
+
+
+    import {currentView} from './store.js'
+
+    export var item;
+
+    const tempTask = {...item};
+
+    async function back(){
+        $currentView = 'ProjectList';
+    }
+
+    async function editProject(){
+        item = tempTask;
+        back();
+    }
+
+</script>
+
+<div id="EditProject">
+
+    <button on:click={back}>Back</button><br>
+
+    <input required placeholder="Nombre del proyecto..." bind:value={tempTask.name}><br>
+    <input required placeholder="Nombre de la tarea..." bind:value={tempTask.project}><br>
+    <input required placeholder="Nombre de la etiqueta..." bind:value={tempTask.tags}><br>
+    <input required placeholder="Tarifa..." bind:value={tempTask.rate}><br>
+    <input required placeholder="Tiempo..." bind:value={tempTask.time}><br>
+
+    <button on:click={editProject}>Guardar</button>
+
+    
+</div>
---
+++ src/editProjectOpcion2.svelte
@@ -0,0 +1,26 @@
+<script>
+/**
+ * En esta opción los cambios se guardan según se cambia el formulario.
+ */
+
+    import {currentView} from './store.js'
+
+    export var item;
+
+    async function back(){
+        $currentView = 'ProjectList';
+    }
+
+</script>
+
+<div id="EditProject">
+
+    <input required placeholder="Nombre del proyecto..." bind:value={item.name}><br>
+    <input required placeholder="Nombre de la tarea..." bind:value={item.project}><br>
+    <input required placeholder="Nombre de la etiqueta..." bind:value={item.tags}><br>
+    <input required placeholder="Tarifa..." bind:value={item.rate}><br>
+    <input required placeholder="Tiempo..." bind:value={item.time}><br>
+
+    <button on:click={back}>Done</button><br>
+
+</div>
