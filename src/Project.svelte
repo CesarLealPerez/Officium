23d9d281b73b9cebbd8e3e34be8be0a9a5732bb0
devel <you@example.com>
2020-11-17 12:58:25 +0100

    Tasktracker

---
+++ src/Project.svelte
@@ -0,0 +1,16 @@
+<script>
+    export var item;
+</script>
+
+<button>
+    jaja
+    <p>{item.name}</p>
+    <p><button on:click=""></button></p>
+</button>
+
+<style>
+    button {
+        display: block;
+        background-color: darksalmon;
+    }
+</style>
---
+++ src/TaskTracker.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { tasks } from './store.js';
+    import Project from './Project.svelte';
+    import NewProject from './NewProject.svelte';
+    //Cambiar "./Project.svelte" por modulo del proyecto
+
+</script>
+
+<div>
+    
+    {#each $tasks as item}    
+        <Project/>    
+    {:else}
+        <NewProject/>
+    {/each}
+</div>
