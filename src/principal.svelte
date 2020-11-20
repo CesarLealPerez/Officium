4c6b94c4f9ed4c9d050cdec9d467f034ed6fa535
Principal <j.soto@disroot.org>
2020-11-20 10:46:32 +0100

    add principal

---
+++ src/principal.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { currentView } from './store.js';
+    import ProjectList from './projectList.svelte';
+    import NewProject from './newProject.svelte';
+    //import EditProject from './editProjectOpcion1.svelte';
+    import Menu from './menu.svelte';
+    //import Task from './task.svelte';
+    
+  
+    async function newProject(){
+      $currentView = 'NewProject';
+    }
+  //lista de vistas posibles partiendo de aqui
+  const views = {
+    'ProjectList' : ProjectList,
+    'NewProject' : NewProject,
+    //'EditProject' : EditProject,
+    'Menu' : Menu,
+    //'Task' : Task,
+  }
+
+  //vista inicial almacenada
+  $currentView = 'ProjectList'; //CAMBIAR POR ProjectList QUE CONTIENE A NewProject
+
+</script>
+
+<section id="drafts">
+  <svelte:component this={views[$currentView]}/>
+</section>
+
+
+
+<style>
+    section {       
+        bottom: 2em;
+        left: 2em;    
+    }    
+</style>
