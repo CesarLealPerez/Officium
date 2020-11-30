23f21344daa3ce0882177c85e21d1c18a231ea0f
devel <you@example.com>
2020-11-30 12:42:12 +0100

    btn limpiar

--- src/ProjectList.svelte
+++ src/ProjectList.svelte
@@ -1,6 +1,6 @@
 <script>
     import { currentView } from './Store.js';
-    import ProjectList from './ProjectList.svelte';
+    /*import ProjectList from './ProjectList.svelte';*/
     import NewProject from './NewProject.svelte';
     import Chronometer from './Chronometer.svelte';
     
@@ -25,6 +25,8 @@
   <div class="card-container short">
 
 </div>
+<!--Boton para limpiar LocalStorage-->
+<button on:click="{()=>{localStorage.clear()}}">Limpiar</button>
 
 {#each $tasks as task}
   <div class="card-container short">
@@ -44,15 +46,15 @@
      </div>
      <div class="col s4 cardDatos">
        <div class="col s12">
-         <p>€58.00</p>
+         <p>{task.rate}</p>
        </div>
        <div class="col s12">
          <p class="tiempoCard"><Chronometer/></p>
        </div>
      </div>
-     <div class="col s2 botonPlay">         
+     <!--div class="col s2 botonPlay">         
        <a href="#"><img class="botonPlayImg" src="img/icon_small_arrow_right.png" alt="BotonPlay"></a>
-     </div>
+     </div-->
    </div>
    </Card>
   </div>
