2c951f18be1e638a5c258603b0ce05217fab6673
Cesar Leal Perez <cesarlealperez@hotmail.com>
2020-12-01 10:24:50 +0100

    doble en numeros click resetea el chrono

--- src/Chronometer.svelte
+++ src/Chronometer.svelte
@@ -76,7 +76,7 @@ function pulse(){
 </svelte:head>
 <!--Cronometro-->
 <div class="chrono">
-    <h1 on:click={pulse} data-chronometer>{hours}:{minutes}:{seconds}</h1>
+    <h1 on:click={pulse} on:dblclick={resetChronometer} data-chronometer>{hours}:{minutes}:{seconds}</h1>
 </div>
     <div class="controls">
         <span id="play" on:click = {playChronometer}><i class="fas fa-play"></i></span>
