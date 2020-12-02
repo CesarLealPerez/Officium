3dfefa7286fe7aedec610844c2f47f8d8074cfac
Rubén Terré <rubenterre@users.noreply.github.com>
2020-12-02 09:16:00 +0100

    Estilos de NewProyect y parte de Principal

--- src/NewProject.svelte
+++ src/NewProject.svelte
@@ -10,9 +10,9 @@ var rate;
         const newProject = 
             {
                 name: taskName,
-                project: projectName,
-                tags: tags,
-                rate: rate,
+                project: "@"+projectName,
+                tags: "#"+tags,
+                rate: rate+"€",
                 time: 0
 
             }
@@ -42,28 +42,38 @@ function comprobar(){
 
 </script>
 
-<p>NewProject</p>
 <svelte:head>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
 </svelte:head>
 
-
-<div class="create-project-container">
-    <h2 id="name">Crea tu tarea:</h2>
-    <input id="taskName" type="text" placeholder="Nombre la tarea" required bind:value={taskName}><br>
-    <input id="projectName" type="text" placeholder="@ Nombre del proyecto" required bind:value={projectName}><br>
-    <input id="tags" type="text" placeholder="# Etiquetas" required bind:value={tags}><br>
-    <input id="rate" type="number" placeholder="Tarifa" required bind:value={rate}>
- 
-    <select id="rate-select" name="ratelist" form="rateform">
-        <option value="euro">Euro(€)</option>
-        <option value="dolar">Dolar($)</option>
-    </select><br>
-
-    <button class="create-project-button" on:click={comprobar}><i class="fas">Crear</i></button><br>
-    <button on:click={goToProjects}><i class="fas fa-arrow-left">Volver a lista de tareas</i></button>
-
+<div class="container">
+    <div class="create-project-container">
+        <h2 id="name">Crea tu tarea:</h2>
+        <input id="taskName"  class="white-text" type="text" placeholder="Nombre la tarea" autocomplete="off" required bind:value={taskName}><br>
+        <input id="projectName" class="white-text"  type="text" placeholder="@ Nombre del proyecto" autocomplete="off" required bind:value={projectName}><br>
+        <input id="tags" class="white-text"  type="text" placeholder="# Etiquetas" autocomplete="off" required bind:value={tags}><br>
+        <input id="rate" class="white-text"  type="number" placeholder="Tarifa" autocomplete="off" required bind:value={rate}>
+     
+        <select id="rate-select" name="ratelist" form="rateform">
+            <option value="euro">Euro(€)</option>
+            <option value="dolar">Dolar($)</option>
+        </select><br>
+    
+        <div class="botones-crear">
+            <div class="row">
+                <div class="col s6 left">
+                    <button class="volver" on:click={goToProjects}>VOLVER A LISTA DE TAREAS</button>
+                </div>
+                <div class="col s6 right">
+                    <button class="create-project-button" on:click={comprobar}>CREAR PROYECTO</button><br>
+                </div>
+            </div>
+        </div>
+        
+    
+    </div>
 </div>
+
 
 <style>
 
